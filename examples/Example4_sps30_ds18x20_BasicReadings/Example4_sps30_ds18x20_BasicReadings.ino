0731dda10e1d978bc6ed14511c443dfb60916c27
paulvha <paulvha@outlook.com>
2019-01-29 21:45:32 +0100

    added ESP8266 support info

--- examples/Example4_sps30_ds18x20_BasicReadings/Example4_sps30_ds18x20_BasicReadings.ino
+++ examples/Example4_sps30_ds18x20_BasicReadings/Example4_sps30_ds18x20_BasicReadings.ino
@@ -1,28 +1,28 @@
 /************************************************************************************
- *  Copyright (c) January 2019, version 1.0     Paul van Haastrecht  
- * 
+ *  Copyright (c) January 2019, version 1.0     Paul van Haastrecht
+ *
  *  =========================  Highlevel description ================================
- *  
- *  This basic reading example sketch will connect to an SPS-30 for getting data and 
+ *
+ *  This basic reading example sketch will connect to an SPS-30 for getting data and
  *  display the available data. It will also try to find an DS18x20 with onewire
  *
  *  =========================  Hardware connections =================================
  *  /////////////////////////////////////////////////////////////////////////////////
  *  ## UART UART UART UART UART UART UART UART UART UART UART UART UART UART UART  ##
  *  /////////////////////////////////////////////////////////////////////////////////
- *  
- *  Sucessfully test has been performed on an ESP32: 
- *  
+ *
+ *  Sucessfully test has been performed on an ESP32:
+ *
  *  Using serial port1, setting the RX-pin(25) and TX-pin(26)
  *  Different setup can be configured in the sketch.
- *  
+ *
  *  SPS30 pin     ESP32
  *  1 VCC -------- VUSB
- *  2 RX  -------- TX  pin 26  
+ *  2 RX  -------- TX  pin 26
  *  3 TX  -------- RX  pin 25
  *  4 Select      (NOT CONNECTED)
  *  5 GND -------- GND
- *  
+ *
  *  Also successfully tested on Serial2 (default pins TX:17, RX: 16)
  *  NO level shifter is needed as the SPS30 is TTL 5V and LVTTL 3.3V compatible
  *  ..........................................................
@@ -30,108 +30,118 @@
  *  Used SerialPort2. No need to set/change RX or TX pin
  *  SPS30 pin     ATMEGA
  *  1 VCC -------- 5V
- *  2 RX  -------- TX2  pin 16  
+ *  2 RX  -------- TX2  pin 16
  *  3 TX  -------- RX2  pin 17
  *  4 Select      (NOT CONNECTED)
- *  5 GND -------- GND 
- *  
+ *  5 GND -------- GND
+ *
  *  Also tested on SerialPort1 and Serialport3 successfully
  *  .........................................................
  *  Failed testing on UNO
- *  Had to use softserial as there is not a separate serialport. But as the SPS30 
+ *  Had to use softserial as there is not a separate serialport. But as the SPS30
  *  is only working on 115K the connection failed all the time with CRC errors.
- *  
+ *
  *  Not tested ESP8266
- *  As the power is only 3V3 (the SPS30 needs 5V)and one has to use softserial, 
- *  I have not tested this.  
- *  
+ *  As the power is only 3V3 (the SPS30 needs 5V)and one has to use softserial,
+ *  I have not tested this.
+ *
  *  //////////////////////////////////////////////////////////////////////////////////
  *  ## I2C I2C I2C  I2C I2C I2C  I2C I2C I2C  I2C I2C I2C  I2C I2C I2C  I2C I2C I2C ##
  *  //////////////////////////////////////////////////////////////////////////////////
  *  NOTE 1:
  *  Using the I2C communication restricts the data that can be retrieved to concentration
  *  mass only. see detail document.
- *  
+ *
  *  NOTE 2:
- *  As documented in the datasheet, make sure to use external 10K pull-up resistor on 
+ *  As documented in the datasheet, make sure to use external 10K pull-up resistor on
  *  both the SDA and SCL lines. Otherwise the communication with the sensor will fail random.
- *    
+ *
  *  ..........................................................
  *  Successfully tested on ESP32
- *  
+ *
  *  SPS30 pin     ESP32
  *  1 VCC -------- VUSB
  *  2 SDA -------- SDA (pin 21)
  *  3 SCL -------- SCL (pin 22)
  *  4 Select ----- GND (select I2c)
- *  5 GND -------- GND 
- *  
+ *  5 GND -------- GND
+ *
+ *  The pull-up resistors should be to 3V3
  *  ..........................................................
  *  Successfully tested on ATMEGA2560
- *  
+ *
  *  SPS30 pin     ATMEGA
  *  1 VCC -------- 5V
  *  2 SDA -------- SDA
- *  3 SCL -------- SCL 
+ *  3 SCL -------- SCL
  *  4 Select ----- GND  (select I2c)
- *  5 GND -------- GND 
- *  
+ *  5 GND -------- GND
+ *
  *  ..........................................................
  *  Successfully tested on UNO R3
- *  
+ *
  *  SPS30 pin     UNO
  *  1 VCC -------- 5V
  *  2 SDA -------- A4
- *  3 SCL -------- A5 
+ *  3 SCL -------- A5
  *  4 Select ----- GND  (select I2c)
- *  5 GND -------- GND 
- *  
- *  When UNO-board is detected the UART code is excluded as that 
- *  does not work on UNO and will save memory. Also some buffers 
- *  reduced and the call to GetErrDescription() is removed to allow 
- *  enough memory.  
+ *  5 GND -------- GND
+ *
+ *  When UNO-board is detected the UART code is excluded as that
+ *  does not work on UNO and will save memory. Also some buffers
+ *  reduced and the call to GetErrDescription() is removed to allow
+ *  enough memory.
  *  ..........................................................
- *  Not tested ESP8266
- *  
+ *  Successfully tested on ESP8266
+ *
+ *  SPS30 pin     External     ESP8266
+ *  1 VCC -------- 5V
+ *  2 SDA -----------------------SDA
+ *  3 SCL -----------------------SCL
+ *  4 Select ----- GND --------- GND  (select I2c)
+ *  5 GND -------- GND --------- GND
+ *
+ *  The pull-up resistors should be to 3V3 from the ESP8266.
+ *
  *   ====  Optional temperature sensor (DS18x20 family) ====
  *
  *             DS18x20           ESP32 thing
  *   ------      GND   ------------- GND
  * |-| 4k7 |--   VCC /red ---------  3V3
- * | ------ 
- * |-----------  data/yellow ------- 4 
- * 
+ * | ------
+ * |-----------  data/yellow ------- 4
+ *
  *  You MUST connect a resistor of 4K7 between data and VCC for pull-up.
- *  
+ *
  * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- *  WARNING!! WARNING !!WARNING!! WARNING !!WARNING!! WARNING !! WARNING!! WARNING !! 
- *  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
- *  The DS18B20 works with VCC between 3 and 5V. It works fine on 3.3V however if connected to 5V 
+ *  WARNING!! WARNING !!WARNING!! WARNING !!WARNING!! WARNING !! WARNING!! WARNING !!
+ *  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *  The DS18B20 works with VCC between 3 and 5V. It works fine on 3.3V however if connected to 5V
  *  and you have an ESP32, you MUST include a level shifter or making a bridge with resistors like below
- *  
- *            -------            ------- 
+ *
+ *            -------            -------
  *  GND ------| 10K  |-----!---- | 5k6 |------  data/yellow from DS18x20
- *            --------     !     -------  
+ *            --------     !     -------
  *                         !
  *                         pin 4 (ESP32)
- * 
+ *
  *  ================================= PARAMETERS =====================================
  *
  *  From line 153 there are configuration parameters for the program
- *  
+ *
  *  ================================== SOFTWARE ======================================
  *  Sparkfun ESP32
- *  
- *    Make sure : 
+ *
+ *    Make sure :
  *      - To select the Sparkfun ESP32 thing board before compiling
  *      - The serial monitor is NOT active (will cause upload errors)
  *      - Press GPIO 0 switch during connecting after compile to start upload to the board
- *  
+ *
  *  DS18X20:
- *  
+ *
  *  For Temperature sensor obtain  https://github.com/PaulStoffregen/OneWire
- *  Code for the DS18x20 is based on the DS18x20 example in the onewire library 
- *  
+ *  Code for the DS18x20 is based on the DS18x20 example in the onewire library
+ *
  *  ================================ Disclaimer ======================================
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -141,7 +151,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *  ===================================================================================
- *  
+ *
  *  NO support, delivered as is, have fun, good luck !!
  */
 
@@ -156,15 +166,15 @@
  *   SERIALPORT             ONLY IF there is NO monitor attached
  *   SERIALPORT1            Arduino MEGA2560, Sparkfun ESP32 Thing : MUST define new pins as defaults are used for flash memory)
  *   SERIALPORT2            Arduino MEGA2560 and ESP32
- *   SERIALPORT3            Arduino MEGA2560 only for now 
- 
+ *   SERIALPORT3            Arduino MEGA2560 only for now
+
  * NOTE: Softserial has been left in as an option, but as the SPS30 is only
  * working on 115K the connection will probably NOT work on any device. */
 /////////////////////////////////////////////////////////////
 #define SP30_COMMS SERIALPORT1
 
 /////////////////////////////////////////////////////////////
-/* define RX and TX pin for softserial and Serial1 on ESP32 
+/* define RX and TX pin for softserial and Serial1 on ESP32
  * can be set to zero if not applicable / needed           */
 /////////////////////////////////////////////////////////////
 #define TX_PIN 26
@@ -181,8 +191,8 @@
 //////////////////////////////////////////////////////////////
 //            DS18x20  configuration                        //
 //////////////////////////////////////////////////////////////
-  
-/* To which pin is the data wire of the DS18x20 connected 
+
+/* To which pin is the data wire of the DS18x20 connected
  * 0 means NO temperature sensor */
 #define TEMP_PIN 4          // see remark in hardware section in begin sketch
 
@@ -204,31 +214,31 @@ byte type_s;
 byte addr[8];
 
 void setup() {
-  
+
   Serial.begin(115200);
 
   serialTrigger("SPS30-Example4: Basic reading + DS18x20. press <enter> to start");
 
   Serial.println(F("Trying to connect"));
-  
+
   // set driver debug level
   sps30.EnableDebugging(DEBUG);
 
   // set pins to use for softserial and Serial1 on ESP32
   if (TX_PIN != 0 && RX_PIN != 0) sps30.SetSerialPin(RX_PIN,TX_PIN);
- 
+
   // Begin communication channel;
   if (sps30.begin(SP30_COMMS) == false) {
     Errorloop("could not initialize communication channel.", 0);
   }
-  
+
   // check for SPS30 connection
   if (sps30.probe() == false) {
     Errorloop("could not probe / connect with SPS30", 0);
   }
   else
     Serial.println(F("Detected SPS30"));
-    
+
   // reset SPS30 connection
   if (sps30.reset() == false) {
     Errorloop("could not reset", 0);
@@ -239,7 +249,7 @@ void setup() {
 
   // check for DS18x20 connection
   init_tmp();
-  
+
   // start measurement
   if (sps30.start() == true)
     Serial.println(F("Measurement started"));
@@ -267,11 +277,11 @@ void loop() {
 void init_tmp(void) {
 
   type_s = 0xf;            // indicate no sensor
-  
+
   if (TEMP_PIN == 0) return;
 
-  Serial.print(F("Try to detect temperature sensor. "));  
-  
+  Serial.print(F("Try to detect temperature sensor. "));
+
   if ( !ds.search(addr)) {
     Serial.println(F("No temperature sensor detected."));
     return;
@@ -312,8 +322,8 @@ float read_Temperature()
   byte data[12];
   float celsius, fahrenheit;
 
-  if (type_s == 0xf) return(0); 
-   
+  if (type_s == 0xf) return(0);
+
   ds.reset();
   ds.select(addr);
   ds.write(0x44, 1);          // start conversion, with parasite power on at the end
@@ -323,7 +333,7 @@ float read_Temperature()
   present = ds.reset();
   ds.select(addr);
   ds.write(0xBE);             // Read Scratchpad
-  
+
   for ( i = 0; i < 9; i++) {  // we need 9 bytes
     data[i] = ds.read();
   }
@@ -378,7 +388,7 @@ void GetDeviceInfo()
   ret = sps30.GetProductName(buf, 32);
   if (ret == ERR_OK)  {
     Serial.print(F("\tProduct name  : "));
-    
+
     if(strlen(buf) > 0)  Serial.println(buf);
     else Serial.println(F("not available"));
   }
@@ -389,7 +399,7 @@ void GetDeviceInfo()
   ret = sps30.GetArticleCode(buf, 32);
   if (ret == ERR_OK)  {
     Serial.print(F("\tArticle code  : "));
-    
+
     if(strlen(buf) > 0)  Serial.println(buf);
     else Serial.println(F("not available"));
   }
@@ -405,54 +415,54 @@ bool read_all()
   static bool header = true;
   uint8_t ret, error_cnt = 0;
   struct sps_values val;
- 
+
   // loop to get data
   do {
 
     ret = sps30.GetValues(&val);
-    
+
     // data might not have been ready
     if (ret == ERR_DATALENGTH){
-        
+
         if (error_cnt++ > 3) {
-          ErrtoMess("Error during reading values: ",ret); 
+          ErrtoMess("Error during reading values: ",ret);
           return(false);
         }
         delay(1000);
     }
-    
+
     // if other error
     else if(ret != ERR_OK) {
-      ErrtoMess("Error during reading values: ",ret); 
+      ErrtoMess("Error during reading values: ",ret);
       return(false);
     }
-    
+
   } while (ret != ERR_OK);
-  
+
   // only print header first time
   if (header) {
     Serial.print(F("-------------Mass -----------    ------------- Number --------------   -Average-"));
-    
+
     if(type_s != 0xf) Serial.println(F(" -Temperature-"));
     else Serial.println();
 
     Serial.print(F("     Concentration [μg/m3]             Concentration [#/cm3]             [μm]"));
-    
+
     if(type_s != 0xf) {
       if (TEMP_TYPE) Serial.println(F("\t\t[*C]"));
       else Serial.println(F("\t\t[*F]"));
     }
     else Serial.println();
-    
+
     Serial.println(F("P1.0\tP2.5\tP4.0\tP10\tP0.5\tP1.0\tP2.5\tP4.0\tP10\tPrtSize\n"));
 
     header = false;
   }
-  
+
   Serial.print(val.MassPM1);
   Serial.print(F("\t"));
   Serial.print(val.MassPM2);
-  Serial.print(F("\t")); 
+  Serial.print(F("\t"));
   Serial.print(val.MassPM4);
   Serial.print(F("\t"));
   Serial.print(val.MassPM10);
@@ -464,24 +474,24 @@ bool read_all()
   Serial.print(val.NumPM2);
   Serial.print(F("\t"));
   Serial.print(val.NumPM4);
-  Serial.print(F("\t")); 
+  Serial.print(F("\t"));
   Serial.print(val.NumPM10);
   Serial.print(F("\t"));
   Serial.print(val.PartSize);
-  
+
   if(type_s != 0xf){
     Serial.print(F("\t\t"));
     Serial.print(read_Temperature());
   }
-  
+
   Serial.print(F("\n"));
 }
 
-/** 
+/**
  *  @brief : continued loop after fatal error
  *  @param mess : message to display
  *  @param r : error code
- *  
+ *
  *  if r is zero, it will only display the message
  */
 void Errorloop(char *mess, uint8_t r)
@@ -492,35 +502,35 @@ void Errorloop(char *mess, uint8_t r)
   for(;;) delay(100000);
 }
 
-/** 
+/**
  *  @brief : display error message
  *  @param mess : message to display
  *  @param r : error code
- *  
+ *
  */
 void ErrtoMess(char *mess, uint8_t r)
 {
   char buf[80];
-  
+
   Serial.print(mess);
 
   sps30.GetErrDescription(r, buf, 80);
   Serial.println(buf);
 }
 
-/** 
+/**
  * serialTrigger prints repeated message, then waits for enter
  * to come in from the serial port.
  */
 void serialTrigger(char * mess)
 {
   Serial.println();
-  
+
   while (!Serial.available()) {
     Serial.println(mess);
     delay(2000);
   }
-  
+
   while (Serial.available())
     Serial.read();
 }
